00a9b712497e73dd7995aa62eb0e3e34f2c9e594
GJdu <briandu8@gmail.com>
2022-03-10 00:08:04 +0000

    added nachonet

---
+++ nacho_net_pytorch.py
@@ -0,0 +1,113 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+
+from torchvision import transforms
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+
+TRAIN_DATA_PATH = "MIT_split/train/"
+TEST_DATA_PATH = "MIT_split/test/"
+
+EPOCHS = 50
+BATCH_SIZE = 64
+LEARNING_RATE = 1e-3
+
+TRANSFORM_IMG = transforms.Compose([
+    transforms.Resize(224),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225] )
+    ])
+
+train_data = ImageFolder(root=TRAIN_DATA_PATH, transform=TRANSFORM_IMG)
+train_dataloader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
+test_data = ImageFolder(root=TEST_DATA_PATH, transform=TRANSFORM_IMG)
+test_dataloader  = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True) 
+
+for X, y in test_dataloader:
+    print(f"Shape of X [N, C, H, W]: {X.shape}")
+    print(f"Shape of y: {y.shape} {y.dtype}")
+    break
+
+# Define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.features = nn.Sequential(
+            
+            nn.Conv2d(3, 64, 7),
+            nn.ReLU(inplace=True),
+
+            nn.MaxPool2d(2),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 128, 3),
+            nn.ReLU(inplace=True),
+
+            nn.MaxPool2d(2),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 256, 3),
+            nn.ReLU(inplace=True),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256*6*6, 8),
+            nn.Softmax()
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+model = NeuralNetwork().to(device)
+print(model)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+for t in range(EPOCHS):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer)
+    test(test_dataloader, model, loss_fn)
+print("Done!")
