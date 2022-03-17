ef12faa97ba9b2e7b566a8dc8e3eebe1e8bcab0f
GJdu <briandu8@gmail.com>
2022-03-17 04:07:35 +0100

    added faster rcnn inference

--- Week2/detectron2_tutorial_inference.py
+++ Week2/detectron2_tutorial_inference.py
@@ -25,9 +25,12 @@ im = np.array(Image.open(dataset_path + 'training/image_02/0000/000000.png'))
 
 #Then, we create a detectron2 config and a detectron2 DefaultPredictor to run inference on this image.
 cfg = get_cfg()
+
+## MASK RCNN
 # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
 cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+
 # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
 predictor = DefaultPredictor(cfg)
@@ -41,4 +44,27 @@ print(outputs["instances"].pred_boxes)
 v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
 out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
 image = Image.fromarray(out.get_image()[:, :, ::-1])
-image.save('detectron2_inference.png',)
+image.save('detectron2_mask_rcnn_inference.png',)
+
+####################################
+####################################
+
+## FASTER RCNN
+# add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
+cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+
+# Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")
+predictor = DefaultPredictor(cfg)
+outputs = predictor(im)
+
+# look at the outputs. See https://detectron2.readthedocs.io/tutorials/models.html#model-output-format for specification
+print(outputs["instances"].pred_classes)
+print(outputs["instances"].pred_boxes)
+
+# We can use `Visualizer` to draw the predictions on the image.
+v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+image = Image.fromarray(out.get_image()[:, :, ::-1])
+image.save('detectron2_faster_rcnn_inference.png',)
