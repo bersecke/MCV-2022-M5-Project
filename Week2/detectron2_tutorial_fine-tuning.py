c1339b6d02329f7ced673c636373d194d587f9d8
IgnacioSan22 <iceamanos1998@gmail.com>
2022-03-17 10:45:49 +0100

    detected boxes and classes

--- Week2/detectron2_tutorial_fine-tuning.py
+++ Week2/detectron2_tutorial_fine-tuning.py
@@ -1,3 +1,4 @@
+from operator import imod
 import torch
 import detectron2
 from detectron2.utils.logger import setup_logger
@@ -13,7 +14,7 @@ from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
-
+from PIL import Image
 dataset_path = '/home/mcv/datasets/KITTI-MOTS/'
 
 ############################
@@ -27,16 +28,34 @@ dataset_path = '/home/mcv/datasets/KITTI-MOTS/'
 
 from detectron2.structures import BoxMode
 
+
+def getItemsFromMask(maskPath):
+    mask_1 = cv2.imread(maskPath,cv2.IMREAD_GRAYSCALE)
+    output = cv2.connectedComponentsWithStats(mask_1, 8, cv2.CV_32S)
+    (numLabels, labels, boxes, centroids) = output
+    mask = np.array(Image.open(maskPath))
+
+    objs = []
+    # print(boxes)
+    for box in boxes[1:]:
+        # print(f'({box[0]},  {box[1]}), ({box[0]+box[2]}, {box[1]+box[3]})')
+        obj_id = mask[box[1] + (box[3] // 2), box[0] + (box[2] // 2)]
+        class_id = obj_id // 1000
+        obj_instance_id = obj_id % 1000
+        objs.append([box[0], box[1], box[0] + box[2], box[1] + box[3], class_id, obj_instance_id])
+
+    return objs
+
+
 def get_KITTIMOTS_dicts(img_dir):
-    json_file = os.path.join(img_dir, "via_region_data.json")
-    with open(json_file) as f:
-        imgs_anns = json.load(f)
+    listOfFiles = list()
+    for (dirpath, dirnames, filenames) in os.walk(img_dir):
+        listOfFiles += [os.path.join(dirpath, file) for file in filenames]
 
     dataset_dicts = []
-    for idx, v in enumerate(imgs_anns.values()):
+    for idx, filename in enumerate(listOfFiles):
         record = {}
         
-        filename = os.path.join(img_dir, v["filename"])
         height, width = cv2.imread(filename).shape[:2]
         
         record["file_name"] = filename
@@ -44,28 +63,30 @@ def get_KITTIMOTS_dicts(img_dir):
         record["height"] = height
         record["width"] = width
       
-        annos = v["regions"]
         objs = []
-        for _, anno in annos.items():
-            assert not anno["region_attributes"]
-            anno = anno["shape_attributes"]
-            px = anno["all_points_x"]
-            py = anno["all_points_y"]
-            poly = [(x + 0.5, y + 0.5) for x, y in zip(px, py)]
-            poly = [p for x in poly for p in x]
-
-            obj = {
-                "bbox": [np.min(px), np.min(py), np.max(px), np.max(py)],
-                "bbox_mode": BoxMode.XYXY_ABS,
-                "segmentation": [poly],
-                "category_id": 0,
-            }
-            objs.append(obj)
+        boxes = getItemsFromMask(filename)
+        for elems in boxes:
+            # assert not anno["region_attributes"]
+            # anno = anno["shape_attributes"]
+            # px = anno["all_points_x"]
+            # py = anno["all_points_y"]
+            # poly = [(x + 0.5, y + 0.5) for x, y in zip(px, py)]
+            # poly = [p for x in poly for p in x]
+            if elems[4] != 10:
+                obj = {
+                    "bbox": [elems[0], elems[1], elems[2], elems[3]],
+                    "bbox_mode": BoxMode.XYXY_ABS,
+                    "segmentation": [None],
+                    "category_id": elems[4],
+                }
+                objs.append(obj)
         record["annotations"] = objs
         dataset_dicts.append(record)
     return dataset_dicts
 
-for d in ["train", "val"]:
-    DatasetCatalog.register("balloon_" + d, lambda d=d: get_balloon_dicts("balloon/" + d))
-    MetadataCatalog.get("balloon_" + d).set(thing_classes=["balloon"])
-balloon_metadata = MetadataCatalog.get("balloon_train")
+# for d in ["train", "val"]:
+#     DatasetCatalog.register("balloon_" + d, lambda d=d: get_balloon_dicts("balloon/" + d))
+#     MetadataCatalog.get("balloon_" + d).set(thing_classes=["balloon"])
+
+
+# balloon_metadata = MetadataCatalog.get("Kitti-Motts train")
--- Week2/test.py
+++ Week2/test.py
@@ -1,3 +1,4 @@
+from binascii import rledecode_hqx
 import tensorflow as tf
 import PIL.Image as Image
 import numpy as np
@@ -12,10 +13,10 @@ import torchvision.transforms as transforms
 import torchvision.transforms.functional as F
 
 
-with open('0000.txt','r') as fd:
-    lines = fd.readlines()
+# with open('0000.txt','r') as fd:
+#     lines = fd.readlines()
 
-line0 = lines[0].split(' ')
+# line0 = lines[0].split(' ')
 
 
 
@@ -29,15 +30,75 @@ dBoxes = []
 # print(boxes)
 for box in boxes[1:]:
     print(f'({box[0]},  {box[1]}), ({box[0]+box[2]}, {box[1]+box[3]})')
+    centerX = box[0] + (box[2] // 2)
+    centerY = box[1] + (box[3] // 2)
+    cv2.circle(img_0,(centerX, centerY), 5, (0,255,0))
     img_0 = cv2.rectangle(img_0, (box[0],  box[1]), (box[0] + box[2], box[1] + box[3]), (255,0,0), 2)
 
 mask = np.array(Image.open("000000.png"))
+print(mask.shape)
+plt.imshow(mask)
+plt.show()
+# np.savetxt('mask.txt',mask, delimiter=' ', fmt = '%.2f')
 obj_ids = np.unique(mask)
+print(obj_ids)
+box = boxes[1]
+centerX = box[0] + (box[2] // 2)
+centerY = box[1] + (box[3] // 2)
+print('Pixel inside box: ', mask[centerY,centerX])
 
-obj_id = obj_ids[1]
+obj_id = mask[centerY,centerX]
 class_id = obj_id // 1000
 obj_instance_id = obj_id % 1000
-print(obj_ids,class_id, obj_instance_id)
-
+print(class_id, obj_instance_id)
+	
 plt.imshow(img_0)
 plt.show()
+
+
+# internal conversion from compressed RLE format to Python RLEs object
+# def rle2bbox(rle, shape):
+#     '''
+#     rle: run-length encoded image mask, as string
+#     shape: (height, width) of image on which RLE was produced
+#     Returns (x0, y0, x1, y1) tuple describing the bounding box of the rle mask
+    
+#     Note on image vs np.array dimensions:
+    
+#         np.array implies the `[y, x]` indexing order in terms of image dimensions,
+#         so the variable on `shape[0]` is `y`, and the variable on the `shape[1]` is `x`,
+#         hence the result would be correct (x0,y0,x1,y1) in terms of image dimensions
+#         for RLE-encoded indices of np.array (which are produced by widely used kernels
+#         and are used in most kaggle competitions datasets)
+#     '''
+    
+#     a = np.fromiter(rle.split(), dtype=np.uint)
+#     a = a.reshape((-1, 2))  # an array of (start, length) pairs
+#     a[:,0] -= 1  # `start` is 1-indexed
+    
+#     y0 = a[:,0] % shape[0]
+#     y1 = y0 + a[:,1]
+#     if np.any(y1 > shape[0]):
+#         # got `y` overrun, meaning that there are a pixels in mask on 0 and shape[0] position
+#         y0 = 0
+#         y1 = shape[0]
+#     else:
+#         y0 = np.min(y0)
+#         y1 = np.max(y1)
+    
+#     x0 = a[:,0] // shape[0]
+#     x1 = (a[:,0] + a[:,1]) // shape[0]
+#     x0 = np.min(x0)
+#     x1 = np.max(x1)
+    
+#     if x1 > shape[1]:
+#         # just went out of the image dimensions
+#         raise ValueError("invalid RLE or image dimensions: x1=%d > shape[1]=%d" % (
+#             x1, shape[1]
+#         ))
+
+#     return x0, y0, x1, y1
+
+# rle = '\Xe<3b;4L4M3N0O001O010O001O10OeFA_7`0`HA_7?bHB\7?cHB]7=cHD\7=bHE\7<dHFY7;gHFW7<TGAY15a7i0ZHYO\7S1_HPO[7V1cHlNY7X1dHkNZ7W1bHmN\7U1aHnN[7U1aHPO[7T1^HSO`7o0ZHWOe7j0WHYOi7[2N1002O0O2O0O2I6D=H7L52M2O0O010O1001N4M3M2M4lNmF]OU9`0lF@W9<kFCX98jFHY94iFJ[92fFN]9NeF1^9JdF6_9FcF9`9CaF=b9^OaFa0b9[O`Fc0\:O001O1O1O000001WE_OY:a0cEB^:>^EFb::[EHf:e00O1O100O1N2O1N2N3M2O1N2N2O2M2M3Ka^?'
+
+# print(rle2bbox(rle, (375, 1242)))
