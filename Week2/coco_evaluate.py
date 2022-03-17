e23ebde0e6d1391547df392b069ff6e5caf0cf4d
GJdu <briandu8@gmail.com>
2022-03-17 14:47:11 +0100

    test pycocotools

--- Week2/coco_evaluate.py
+++ Week2/coco_evaluate.py
@@ -1,3 +1,6 @@
-from detectron2.evaluation import COCOEvaluator
+from detectron2.utils.logger import setup_logger
 
-evaluator = COCOEvaluator("balloon_val", output_dir="./output")
+setup_logger
+
+from detectron2.data.datasets import register_coco_instances
+from detectron2.engine import DefaultTrainer
