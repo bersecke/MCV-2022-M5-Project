ca9216421a6bf36f42ef9be14c99222b1c90d527
GJdu <briandu8@gmail.com>
2022-03-18 17:21:48 +0100

    prepare for rebase

--- Week2/coco_evaluate.py
+++ Week2/coco_evaluate.py
@@ -4,3 +4,19 @@ setup_logger
 
 from detectron2.data.datasets import register_coco_instances
 from detectron2.engine import DefaultTrainer
+
+import os
+import pickle
+
+from utils import *
+
+config_file_path = "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
+
+def my_dataset_function():
+  ...
+  return list[dict] in the following format
+
+from detectron2.data import DatasetCatalog
+DatasetCatalog.register("my_dataset", my_dataset_function)
+# later, to access the data:
+data: List[Dict] = DatasetCatalog.get("my_dataset")
