284fd1556dbf63385c983c58e029286c37250011
Eric Henriksson Martí <eckehenrikssonmarti@gmail.com>
2022-03-19 17:11:16 +0100

    Changed dataset path (2)

--- Week2/customization_support.py
+++ Week2/customization_support.py
@@ -37,10 +37,10 @@ def get_KITTIMOTS_dicts(data_type):
     Registers the KITTI-MOTS dataset to detectron2
     """
     if data_type == 'train':
-        img_dir = '../KITTI-MOTS/instances/'
+        img_dir = '/home/mcv/datasets/KITTI-MOTS/instances/'
         sequences = ['0000', '0001', '0002', '0003', '0004', '0005', '0006', '0007', '0007', '0008', '0009', '0010', '0011']
     elif data_type == 'valid':
-        img_dir = '../KITTI-MOTS/instances/'
+        img_dir = '/home/mcv/datasets/KITTI-MOTS/instances/'
         sequences = ['0012', '0013', '0014', '0015', '0016', '0017', '0018', '0019', '0020']
 
     listOfFiles = list()
