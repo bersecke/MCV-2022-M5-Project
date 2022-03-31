04993026786659649d1229306b8ad02267b615da
Brian <briandu8@gmail.com>
2022-04-01 00:59:46 +0200

    Update networks.py

--- Week4/networks.py
+++ Week4/networks.py
@@ -88,3 +88,40 @@ class TripletNet(nn.Module):
 
     def get_embedding(self, x):
         return self.embedding_net(x)
+
+class EmbeddingNet(nn.Module):
+    def __init__(self):
+        super(EmbeddingNet, self).__init__()
+        self.convnet = nn.Sequential(nn.Conv2d(1, 32, 5), nn.PReLU(),
+                                     nn.MaxPool2d(2, stride=2),
+                                     nn.Conv2d(32, 64, 5), nn.PReLU(),
+                                     nn.MaxPool2d(2, stride=2))
+
+        self.fc = nn.Sequential(nn.Linear(64 * 4 * 4, 256),
+                                nn.PReLU(),
+                                nn.Linear(256, 256),
+                                nn.PReLU(),
+                                nn.Linear(256, 2)
+                                )
+
+    def forward(self, x):
+        output = self.convnet(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc(output)
+        return output
+
+    def get_embedding(self, x):
+        return self.forward(x)
+
+
+class EmbeddingNetL2(EmbeddingNet):
+    def __init__(self):
+        super(EmbeddingNetL2, self).__init__()
+
+    def forward(self, x):
+        output = super(EmbeddingNetL2, self).forward(x)
+        output /= output.pow(2).sum(1, keepdim=True).sqrt()
+        return output
+
+    def get_embedding(self, x):
+        return self.forward(x)
