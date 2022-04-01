0d5e927e23ccd0e8edac1f642051b255bcb1c4c4
Brian <briandu8@gmail.com>
2022-04-01 05:15:49 +0200

    added new mit split dataset class

--- Week4/image_loader.py
+++ Week4/image_loader.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import pickle
+import torch
 from torch.utils.data.dataset import Dataset  # For custom datasets
 
 
@@ -32,3 +33,62 @@ class MITSplitDataSet(Dataset):
 
     def __len__(self):
         return self.data_len
+
+class MITSplitDataset_v2(Dataset):
+    def __init__(self, train_path, test_path, train_labels_path, test_labels_path,train):
+        # Transforms
+        self.to_tensor = transforms.ToTensor()
+        self.transform=None
+        # Read the csv file
+        self.train=train
+        if self.train:
+            train_images_filenames = pickle.load(open(train_path,'rb'))
+            self.train_images_filenames = ['..' + n[15:] for n in train_images_filenames]
+            self.labels = pickle.load(open(train_labels_path,'rb'))
+            self.train_data =[] 
+            
+            print("printing train data length CUHK")
+            print(len(self.train_data_info.index))
+
+            for img in train_images_filenames:
+                try : 
+                    self.train_data.append(self.to_tensor(Image.open(img))) 
+                except : 
+                    print(img)
+            
+
+            self.train_data = torch.stack(self.train_data)
+            self.train_labels = pickle.load(open(train_labels_path,'rb'))
+
+            self.train_data_len = len(self.train_images_filenames)
+
+        else :
+            test_images_filenames = pickle.load(open(test_path,'rb'))
+            self.train_images_filenames = ['..' + n[15:] for n in test_images_filenames]
+            self.labels = pickle.load(open(train_labels_path,'rb'))
+            self.test_data =[] 
+            for img in test_images_filenames:
+                try : 
+                    self.test_data.append(self.to_tensor(Image.open(img))) 
+                except : 
+                    print(img)  
+
+            self.test_data = torch.stack(self.test_data)
+            self.test_labels = pickle.load(open(test_labels_path,'rb'))
+            
+            self.test_data_len = len(self.test_images_filenames)
+            
+
+    def __getitem__(self, index):
+        if self.train:
+            img, target = self.train_data[index], self.train_labels[index]
+        else:
+            img, target = self.test_data[index], self.test_labels[index]
+
+        return (img,target)
+
+    def __len__(self):
+        if self.train :
+            return self.train_data_len
+        else :
+            return self.test_data_len
